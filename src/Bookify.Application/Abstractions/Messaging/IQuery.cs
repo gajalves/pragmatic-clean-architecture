399769333ca8f2ba9034d90b051de7d9fe46af53
gajalves <ga.jaimealves@gmail.com>
2024-01-18 00:35:42 -0300

    feat: Adding the required abstractions for CQRS

---
+++ src/Bookify.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+
+}
