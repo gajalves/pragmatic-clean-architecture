448ebe482fe7ad8bdf2527acdbbd2d42846c4b43
gajalves <ga.jaimealves@gmail.com>
2024-01-19 00:37:13 -0300

    feat: Implementing Queries with Dapper

---
+++ src/Bookify.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,5 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+public sealed record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>;
