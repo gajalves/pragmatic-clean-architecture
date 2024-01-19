a707a9d30cce0e65b7b6ad76540635f29b59c1be
gajalves <ga.jaimealves@gmail.com>
2024-01-18 23:45:06 -0300

    feat: Defining a domain event handler

---
+++ src/Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Bookify.Application.Abstractions.Email;
+
+public interface IEmailService
+{
+    Task SendAsync(Domain.Users.ValueObjects.Email recipient, string subject, string body);
+}
